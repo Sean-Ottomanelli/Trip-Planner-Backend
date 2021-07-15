c99f5cfb34bdef2079e7b916abfd2a0ba0002d75
github username <github email address>
2021-07-15 09:41:18 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,29 +74,31 @@ m4 = Marker.create({
 m5 = Marker.create({
     user_id: u1.id,
     visited: true,
-    user_rating: 2,
-    name: "LMCC's Arts Center at Governor's Island",
+    user_rating: 5,
+    name: "Tulane University",
+    latitude: 29.934945674627386,
+    longitude: -90.12310066203958,
     category: "Attraction",
-    image: "https://cdn.businessyab.com/assets/uploads/ceeb727bde7ed7791c296a5be62fb0ef_-united-states-new-york-new-york-county-manhattan-andes-road-110-lmccs-arts-center-at-governors-island.jpg",
-    latitude: 40.69255380369747,
-    longitude: -74.01571330476163
+    image: "https://www.venuesolutionsgroup.com/wp-content/uploads/2020/02/gibsoncircle.jpg"
+    
 })
 
 m6 = Marker.create({
     user_id: u1.id,
-    visited: true,
-    user_rating: 1,
-    name: "The Bindery",
-    category: "Attraction",
-    image: "https://sec.report/Document/0001035443-19-000041/q418bindery.jpg",
-    latitude: 40.74180695062785,
-    longitude: -73.93716540337736
+    visited: false,
+    urgency: 4,
+    name: "Waianapanapa State Park",
+    latitude: 20.786459556988213,
+    longitude: -156.0030289601703,
+    category: "Hike",
+    image: "https://wpcdn.us-east-1.vip.tn-cloud.net/www.hawaiimagazine.com/content/uploads/2020/12/Black-Sand-Flattened-copy.jpg"
+    
 })
 
 m7 = Marker.create({
     user_id: u1.id,
     visited: false,
-    urgency: 4,
+    urgency: 2,
     name: "Statue of Liberty",
     image: "https://www.history.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY1MTc1MTk3ODI0MDAxNjA5/topic-statue-of-liberty-gettyimages-960610006-promo.jpg",
     category: "Attraction",
@@ -106,32 +108,57 @@ m7 = Marker.create({
 
 m8 = Marker.create({
     user_id: u1.id,
-    visited: false,
-    urgency: 3,
-    name: "Columbia Business School",
-    category: "Attraction",
-    latitude: 40.82088791703065,
-    longitude: -73.95297702649518
+    visited: true,
+    user_rating: 4,
+    name: "Captain Cook Monument Trail",
+    latitude: 19.491922960964153,
+    longitude: -155.91800056469876,
+    category: "Hike",
+    image: "https://www.fair-wind.com/wp-content/uploads/Fair-Wind-Captain-Cook-Monument-1920x.jpg"
 })
 
 m9 = Marker.create({
     user_id: u1.id,
-    visited: true,
-    user_rating: 5,
-    name: "Brown University Engineering Research Center",
-    category: "Attraction",
-    latitude: 41.826369806129,
-    longitude: -71.39829755530644
+    visited: false,
+    urgency: 4,
+    name: "Halemau‘u Trail (Switchback Trail)",
+    latitude: 20.751964847936655,
+    longitude: -156.22941200268403,
+    category: "Hike",
+    image: "https://lh3.googleusercontent.com/proxy/JxY5MNluZkf1o-8tamPZWs9T_oiPdTiYq_FgMChBjl3iuVS9mZC6U9EZmnV3DibdVW5st4T4qOFz2Fi-b4kUzCZFBu__vA_aBlXL0DwtUbqdwRzqD4qIYA"
 })
 
 m10 = Marker.create({
     user_id: u1.id,
     visited: false,
-    urgency: 5,
-    name: "Alexandria Center for Life Sciences",
+    urgency: 4,
+    name: "Pā Ka'oao Trail",
+    latitude: 20.714166440819174,
+    longitude: -156.25034423741727,
+    category: "Hike",
+    image: "https://cdn-assets.alltrails.com/uploads/photo/image/20112747/extra_large_6595a8ed09c910f383eddaa8017771be.jpg"
+})
+
+m11 = Marker.create({
+    user_id: u1.id,
+    visited: true,
+    user_rating: 5,
+    name: "Bike Maui",
+    latitude: 20.913505134988977,
+    longitude: -156.32260442572897,
     category: "Attraction",
-    latitude: 40.739521474282554,
-    longitude: -73.9737136052805
+    image: "https://www.mauiticketsforless.com/wp-content/uploads/2016/03/Mountain-Riders-Sunrise-Bike-Tour-2.jpg"
+})
+
+m12 = Marker.create({
+    user_id: u1.id,
+    visited: true,
+    user_rating: 3,
+    name: "Cafe Du Monde",
+    latitude: 29.959827343955077,
+    longitude: -90.05995235990375,
+    category: "Restaurant",
+    image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/cafe-du-monde-new-orleans-french-quarter-ellis-c-baldwin.jpg"
 })
 
 puts "Markers marked"
@@ -144,7 +171,13 @@ t1 = Trip.create({
 
 t2 = Trip.create({
     user_id: u1.id,
-    name: "Providence 2022",
+    name: "New Orleans 2019",
+    completed: true
+})
+
+t3 = Trip.create({
+    user_id: u1.id,
+    name: "Maui & Big Island 2021",
     completed: false
 })
 
@@ -165,23 +198,18 @@ d3 = Destination.create({
     marker_id: m3.id
 })
 
-d3 = Destination.create({
-    trip_id: t1.id,
-    marker_id: m3.id
-})
-
 d4 = Destination.create({
     trip_id: t1.id,
     marker_id: m4.id
 })
 
 d5 = Destination.create({
-    trip_id: t1.id,
+    trip_id: t2.id,
     marker_id: m5.id
 })
 
 d6 = Destination.create({
-    trip_id: t1.id,
+    trip_id: t3.id,
     marker_id: m6.id
 })
 
@@ -191,18 +219,28 @@ d7 = Destination.create({
 })
 
 d8 = Destination.create({
-    trip_id: t1.id,
+    trip_id: t3.id,
     marker_id: m8.id
 })
 
 d9 = Destination.create({
-    trip_id: t2.id,
+    trip_id: t3.id,
     marker_id: m9.id
 })
 
 d10 = Destination.create({
-    trip_id: t1.id,
+    trip_id: t3.id,
     marker_id: m10.id
+})
+
+d11 = Destination.create({
+    trip_id: t3.id,
+    marker_id: m11.id
+})
+
+d12 = Destination.create({
+    trip_id: t2.id,
+    marker_id: m12.id
 })
 
 puts "You're going places, literally. Destinations seeded."
